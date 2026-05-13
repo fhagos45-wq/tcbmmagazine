ec557264c0a2e7559ff90458733958701cf2c964
Filmon Hagos <fhagos45@gmail.com>
2026-05-13 17:04:54 +0300

    Create angelina.js

---
+++ angelina.js
@@ -0,0 +1,6 @@
+
+const PROJECT_URL = 'https://dplouhkwmyrenhkzqxup.supabase.co'; 
+const ANON_KEY = 'sb_publishable_z3gvIQGoxobO3jwqtwXCiw_irgvm6OB';
+
+
+const supabase = supabase.createClient(PROJECT_URL, ANON_KEY);
